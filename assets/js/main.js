fd69843a4cec14d8a0357d7cec7990444b8719f5
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-04-30 15:01:57 +0600

    update

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+(function($) {
+"use strict";
+
+/*------------------------------------------------------------------
+[Table of contents]
+
+
+-------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------
+CUSTOM PRE DEFINE FUNCTION
+------------------------------------------------------------*/
+/* is_exist() */
+jQuery.fn.is_exist = function(){
+  return this.length;
+}
+
+
+$(function(){
+
+  $('.mas-demo-slider').slick({
+    infinite: true,
+    slidesToShow: 5, 
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+
+});/*End document ready*/
+
+
+})(jQuery);
+
+
+
+
+
+
