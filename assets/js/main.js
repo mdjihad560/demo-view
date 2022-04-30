885502a6563ad7677286f329b4e8f3ee1aab25c2
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-04-30 22:54:12 +0600

    done: all demo slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ jQuery.fn.is_exist = function(){
 
 $(function(){
 
-  $('.mas-demo-slider').slick({
+  $('.mas-demo-slider1').slick({
     infinite: true,
     slidesToShow: 5, 
     slidesToScroll: 1,
@@ -29,19 +29,35 @@ $(function(){
     speed: 3000,
     pauseOnHover: false,
     cssEase: 'linear',
+    pauseOnHover:true,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1400,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       },
@@ -50,6 +66,97 @@ $(function(){
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+
+  $('.mas-demo-slider2').slick({
+    rtl: true,
+    infinite: true,
+    slidesToShow: 5, 
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3000,
+    pauseOnHover: false,
+    cssEase: 'linear',
+    pauseOnHover:true,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+
+  $('.mas-demo-slider3').slick({
+    infinite: true,
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
         }
       }
 
